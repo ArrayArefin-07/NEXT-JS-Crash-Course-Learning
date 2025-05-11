90cdbdd5736eaa2e7cc7eb5bfb2464028317e424
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-05-11 15:14:12 +0600

    Added Repo to remote" -m "learn page & layout

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='mt-10'>
+      <div>
+      This is About US Page
+      </div>
+    </main>
+  )
+}
+
+export default page
