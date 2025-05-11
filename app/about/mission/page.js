92a1694434c7651d98a80067998aa1457b9d0613
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-05-11 15:59:35 +0600

    added Roution & nevigation
    revised Routing and nevigation

---
+++ app/about/mission/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='mt-10'>
+      <div>
+      This Mission Page
+      </div>
+    </main>
+  )
+}
+
+export default page
