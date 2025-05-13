be0cb28ba6a61cba079a7d1c769f056cc983c275
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-05-14 01:14:07 +0600

    learning dynamic data fetching

---
+++ app/posts/[id]/page.js
@@ -0,0 +1,17 @@
+import getPost from "@/lib/getpost";
+import React from "react";
+import Posts from "../page";
+
+const PostPage = async ({params}) => {
+  const { id } = params;
+  const post = await getPost(id);
+
+  return (
+    <div className="mt-6">
+      <h2 className="text-blue-500">{post.title}</h2>
+      <p className="mt-3">{post.body}</p>
+    </div>
+  );
+};
+
+export default PostPage;
