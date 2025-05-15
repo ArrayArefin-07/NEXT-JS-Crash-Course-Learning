a8010325da6c07844aa98fb14fc77ed031db6062
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-05-16 00:31:22 +0600

    learn prograssive rendering
    for data fetching

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,6 +1,8 @@
 import getPost from "@/lib/getpost";
-import React from "react";
+import React, { Suspense } from "react";
 import Posts from "../page";
+import getPostComments from "@/lib/getPostComment";
+import Comments from "@/app/components/Comments";
 
 export async function generateMetadata({ params }) {
   const { id } = params;
@@ -13,12 +15,21 @@ export async function generateMetadata({ params }) {
 
 const PostPage = async ({ params }) => {
   const { id } = params;
-  const post = await getPost(id);
+  const postPromise = getPost(id);
+  const commentsPromise = getPostComments(id);
+
+  // const [post, comments] = await Promise.all([postPromise, commentsPromise]);
+const post = await postPromise;
 
   return (
     <div className="mt-6">
       <h2 className="text-blue-500">{post.title}</h2>
       <p className="mt-3">{post.body}</p>
+      <hr></hr>
+      <Suspense fallback="<h1>Loading comments...</h1>">
+        <Comments promise={commentsPromise} />
+      </Suspense>
+      
     </div>
   );
 };
